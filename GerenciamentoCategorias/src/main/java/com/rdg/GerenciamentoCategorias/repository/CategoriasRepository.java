a46310407778de2ed4592aa2a1d474432a350b22
rodrigo-augusto <rodrigo.silva.110805@gmail.com>
2026-03-24 06:29:05 -0300

    models, repository e readme add

---
+++ GerenciamentoCategorias/src/main/java/com/rdg/GerenciamentoCategorias/repository/CategoriasRepository.java
@@ -0,0 +1,8 @@
+package com.rdg.GerenciamentoCategorias.repository;
+
+import com.rdg.GerenciamentoCategorias.models.CategoriasModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriasRepository extends JpaRepository<CategoriasModel, Long> {
+
+}
